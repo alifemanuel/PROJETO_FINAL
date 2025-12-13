dd2749c531b9accdf191db3befea734360f41cfc
Alif Emanuel <70676382+alifemanuel@users.noreply.github.com>
2025-12-13 13:44:29 +0000

    Commit inicial

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import styles from "./Header.module.css";
+
+export function Header() {
+
+  return (
+    <div className={styles.container}>
+      <div>
+          <h1>Sistema de ensino</h1> 
+      </div>
+
+    </div>
+  );
+}
